8c4252c24a93570248002ecd97c3a2f36842421b
Stephen Wool <stevewool7@gmail.com>
2019-03-01 21:07:39 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -6,3 +6,8 @@ function updatePlaylist(playlist, artist, song) {
   playlist[artist] = song
   return playlist
 }
+
+function removeFromPlaylist(playlist, artist) {
+  delete playlist.artist
+  return playlist
+}
